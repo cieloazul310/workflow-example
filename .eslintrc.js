88b26e71113d6060f75a00d01ebab0c21fff9c96
cieloazul310 <cieloazul310@gmail.com>
2022-09-27 20:59:18 +0900

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,18 @@
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "./tsconfig.eslint.json",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
   },
 };
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function hoge(a: number, b: number) {
+  return a + b;
+}
+
+export default hoge;
