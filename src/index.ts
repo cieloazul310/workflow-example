dfa51b84f7e6fa79d8d6444e8731485d75284b05
cieloazul310 <cieloazul310@gmail.com>
2022-09-29 20:52:31 +0900

    test: add jest

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-function add(...n: number[]) {
+export function add(...n: number[]) {
   return n.reduce((accum, curr) => accum + curr, 0);
 }
-
-export default add;
 
 export function multiply(...n: number[]) {
   return n.reduce((accum, curr) => accum * curr, 1);
